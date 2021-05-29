761425e33a4338167b1695a0c39bd98a6dde256a
adriana <adrianaramona.lazar@gmail.com>
2021-05-29 20:54:57 +0300

    continue

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -4,8 +4,12 @@ import '../../App.css';
 export default function Home() {
   return (
     <>
-      <div className='home'> 
-      <p> Adrianaa ajsljcdlksnjkdn </p>
+      <div className='home' id="home"> 
+        <div className="text-info">
+          <h2>Hi there, I'm </h2>
+          <h1>Adriana Lazar</h1>
+          <h3>Aspiring <span> </span></h3>
+        </div>
       
       
       </div>
